ae494a70f8971d17734a7d4e95ddc3a5541ba2cb
Dalon Lobo <dalonlobo@gmail.com>
2019-01-03 09:50:43 +0000

    main program added

---
+++ main/configuration.py
@@ -0,0 +1,51 @@
+import argparse
+import numpy as np
+
+parser = argparse.ArgumentParser()    # make parser
+
+
+# get arguments
+def get_config():
+    config, unparsed = parser.parse_known_args()
+    return config
+
+
+# return bool type of argument
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+# Arguments
+data_arg = parser.add_argument_group('Data')
+data_arg.add_argument('--random_state', type=int, default=123)
+data_arg.add_argument('--tdsv', type=str2bool, default=False, help="text dependent or not")
+data_arg.add_argument('--sr', type=int, default=16000, help="sampling rate")
+data_arg.add_argument('--nfft', type=int, default=512, help="fft kernel size")
+data_arg.add_argument('--window', type=float, default=0.025, help="window length") # 25ms
+data_arg.add_argument('--hop', type=float, default=0.01, help="hop size") # 10ms
+data_arg.add_argument('--tdsv_frame', type=int, default=160, help="frame number of utterance of tdsv")
+data_arg.add_argument('--tisv_frame', type=int, default=160, help="max frame number of utterances of tdsv")
+data_arg.add_argument('--tisv_frame_min', type=int, default=50, help="min frame length to confirm with 'd-vector v-3 model'")
+
+# Model Parameters
+model_arg = parser.add_argument_group('Model')
+model_arg.add_argument('--hidden', type=int, default=768, help="hidden state dimension of lstm")
+model_arg.add_argument('--proj', type=int, default=256, help="projection dimension of lstm")
+model_arg.add_argument('--num_layer', type=int, default=3, help="number of lstm layers")
+model_arg.add_argument('--restore', type=str2bool, default=False, help="restore model or not")
+model_arg.add_argument('--model_num', type=int, default=3, help="number of ckpt file to load")
+
+# Main program parameters
+main_arg = parser.add_argument_group('Main')
+main_arg.add_argument('--number_of_speakers', type=int, default=2, help="Number of speakers")
+main_arg.add_argument('--log_path', type=str, default='main.logs')
+main_arg.add_argument('--audio_file', type=str, default='xxx.wav', help="Enter the absolute path to audio file")
+main_arg.add_argument('--model_path', type=str, default='models/model.ckpt-46', help="model directory to save or load")
+
+
+config = get_config()
+print(config)           # print all the arguments
---
+++ main/main.py
@@ -0,0 +1,178 @@
+# All imports
+import os
+import logging
+import datetime
+import json
+
+import librosa
+import pysrt
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow.contrib import rnn
+from utils import normalize, loss_cal, optim
+from sklearn.preprocessing import normalize as sk_normalize
+from sklearn.cluster import KMeans
+from scipy.ndimage.filters import gaussian_filter
+from configuration import get_config
+
+config = get_config()
+log_file = os.path.abspath(config.log_path)
+logging.basicConfig(
+    filename=log_file,
+    level=logging.DEBUG,
+    format="%(asctime)s:%(levelname)s:%(message)s"
+    )
+print(f'Log path: {log_file}')
+
+def main():
+    # Changing to 25, which will give slightly better intervals, 20 gives very short intervals
+    vad_threshold = 25 # threshold for voice activity detection
+
+    # Data prep
+    # I'm saving only 2 embeddings i.e. first and last tisv_frames for given interval in an audio. So each .npy
+    # embedding file will have a shape of (2, 256)
+    tf.reset_default_graph()
+    batch_size = 2 # Fixing to 2 since we take 2 for each interval #utter_batch.shape[1]
+    verif = tf.placeholder(shape=[None, batch_size, 40], dtype=tf.float32)  # verification batch (time x batch x n_mel)
+    batch = tf.concat([verif,], axis=1)
+    # embedding lstm (3-layer default)
+    with tf.variable_scope("lstm"):
+        lstm_cells = [tf.contrib.rnn.LSTMCell(num_units=config.hidden, num_proj=config.proj) for i in range(config.num_layer)]
+        lstm = tf.contrib.rnn.MultiRNNCell(lstm_cells)    # make lstm op and variables
+        outputs, _ = tf.nn.dynamic_rnn(cell=lstm, inputs=batch, dtype=tf.float32, time_major=True)   # for TI-VS must use dynamic rnn
+        embedded = outputs[-1]                            # the last ouput is the embedded d-vector
+        embedded = normalize(embedded)                    # normalize
+    config_tensorflow = tf.ConfigProto(
+            device_count = {'GPU': 0}
+        )
+    saver = tf.train.Saver(var_list=tf.global_variables())
+    # Extract embeddings
+    # Each embedding saved file will have (2, 256)
+    with tf.Session(config=config_tensorflow) as sess:
+        tf.global_variables_initializer().run()
+        saver.restore(sess, config.model_path)
+        logging.info("loading audio")
+        audio_path = config.audio_file
+        utter, sr = librosa.core.load(audio_path, sr=config.sr)        # load audio
+        utter_min_len = (config.tisv_frame_min * config.hop + config.window) * sr    # lower bound of utterance length
+        # Get the duration
+        duration = librosa.get_duration(utter, sr)
+        # Duration of each window
+        duration_per_frame = (duration / utter.shape[0])
+        logging.info(f'Duration: {duration}\nDuration per frame: {duration_per_frame}s\nMin length of utterance: {utter_min_len * duration_per_frame}s')
+        tisv_frame_duration_s = utter_min_len * duration_per_frame
+        intervals = librosa.effects.split(utter, top_db=vad_threshold)         # voice activity detection
+
+        all_data = []
+        logging.info('Converting intervals to embeddings')
+        selected_intervals_idx = []
+        for idx, current_interval in enumerate(intervals):
+            if (current_interval[1]-current_interval[0]) > utter_min_len:
+                # Save these selected intervals, as shorter ones are ignored
+                selected_intervals_idx.append(idx)
+                utterances_spec = []
+                utter_part = utter[current_interval[0]:current_interval[1]]         # save first and last 160 frames of spectrogram.
+                S = librosa.core.stft(y=utter_part, n_fft=config.nfft,
+                                      win_length=int(config.window * sr), hop_length=int(config.hop * sr))
+                S = np.abs(S) ** 2
+                mel_basis = librosa.filters.mel(sr=sr, n_fft=config.nfft, n_mels=40)
+                S = np.log10(np.dot(mel_basis, S) + 1e-6)           # log mel spectrogram of utterances
+                utterances_spec.append(S[:, :config.tisv_frame])
+                utterances_spec.append(S[:, -config.tisv_frame:])
+                utterances_spec = np.array(utterances_spec)
+                utter_batch = np.transpose(utterances_spec, axes=(2,0,1))     # transpose [frames, batch, n_mels]
+
+                data = sess.run(embedded, feed_dict={verif:utter_batch})
+                all_data.extend(data)
+    data = np.array(all_data)
+
+    # # Spectral clustering
+    # cossine similarity
+    similarity = np.dot(data, data.T)
+    # squared magnitude of preference vectors (number of occurrences) (diagonals are ai*ai)
+    square_mag = np.diag(similarity)
+    # inverse squared magnitude
+    inv_square_mag = 1 / square_mag
+    # if it doesn't occur, set it's inverse magnitude to zero (instead of inf)
+    inv_square_mag[np.isinf(inv_square_mag)] = 0
+    # inverse of the magnitude
+    inv_mag = np.sqrt(inv_square_mag)
+    # cosine similarity (elementwise multiply by inverse magnitudes)
+    cosine = similarity * inv_mag
+    A =  cosine.T * inv_mag
+    # Fill the diagonals with very large negative value
+    np.fill_diagonal(A, -1000)
+    # Fill the diagonals with the max of each row
+    np.fill_diagonal(A, A.max(axis=1))
+    # final step in cossine sim
+    A = (1-A)/2
+    # Gaussian blur
+    sigma = 0.5 # we will select sigma as 0.5
+    A_gau = gaussian_filter(A, sigma)
+    # Thresholding using multiplier = 0.01
+    threshold_multiplier = 0.01
+    A_thresh = A_gau * threshold_multiplier
+    # Symmetrization
+    A_sym = np.maximum(A_thresh, A_thresh.T)
+    # Diffusion
+    A_diffusion = A_sym * A_sym.T
+    # Row-wise matrix Normalization
+    Row_max = A_diffusion.max(axis=1).reshape(1, A_diffusion.shape[0])
+    A_norm = A_diffusion / Row_max.T
+    # Eigen decomposition
+    eigval, eigvec = np.linalg.eig(A_norm)
+    # Since eigen values cannot be negative for Positive semi definite matrix, the numpy returns negative values, converting it to positive
+    eigval = np.abs(eigval)
+    # reordering eigen values
+    sorted_eigval_idx = np.argsort(eigval)[::-1]
+    sorted_eigval = np.sort(eigval)[::-1]
+    # For division according to the equation
+    eigval_shifted = np.roll(sorted_eigval, -1)
+    # Thresholding eigen values because we don't need very low eigan values due to errors
+    eigval_thresh = 0.1
+    sorted_eigval = sorted_eigval[sorted_eigval > eigval_thresh]
+    eigval_shifted = eigval_shifted[:sorted_eigval.shape[0]]
+    # Don't take the first value for calculations, if first value is large, following equation will return k=1, and we want more than one clusters
+    # Get the argmax of the division, since its 0 indexed, add 1
+    k = np.argmax(sorted_eigval[1:]/eigval_shifted[1:]) + 2
+    logging.debug(f'Number of Eigen vectors to pick: {k}')
+    # Get the indexes of eigen vectors
+    idexes = sorted_eigval_idx[:k]
+    A_eigvec = eigvec[:, idexes]
+    A_eigvec = A_eigvec.astype('float32')
+
+    # # K-Means offline clustering
+    A_eigvec_norm = sk_normalize(A_eigvec) # l2 normalized
+    kmeans = KMeans(n_clusters=config.number_of_speakers, init='k-means++', random_state=config.random_state)
+    kmeans.fit(A_eigvec)
+    labels = kmeans.labels_
+    json_data = []
+    for idx, i in enumerate(selected_intervals_idx):
+        start = str(datetime.timedelta(seconds = intervals[i][0] * duration_per_frame))
+        end = str(datetime.timedelta(seconds = intervals[i][1] * duration_per_frame))
+        speaker = labels[idx*2]
+        if labels[idx*2] != labels[(idx*2)+1]:
+            speaker = 'OL' # possible overlap
+        json_data.append({
+            'start': start,
+            'end': end,
+            'speaker': str(speaker)
+        })
+    # Save the output to json
+    with open(config.output_path, 'w') as f:
+        json.dump(json_data, f, indent=4)
+
+if __name__ = "__main__":
+    """
+    Speaker diarization program:
+    input: audio file
+    output: json with following format:
+            {
+                'start': start-time,
+                'end': end-time,
+                'speaker': speaker
+            }
+    """
+    main()
